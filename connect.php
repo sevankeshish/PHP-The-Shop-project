c2ce9991db498f69ba050d597380dd848d0ee8e3
Sevan Keshish <35175803+sevankeshish@users.noreply.github.com>
2022-02-16 00:03:34 +0330

    debugging connect.php & insertUser.php

--- connect.php
+++ connect.php
@@ -4,12 +4,12 @@ $user="root";
 $pass="";
 $dbname="marketdb";
 
-$dsn="mysql:host=$server;dbname";
+$dsn="mysql:host=$server;dbname=$dbname";
     try{
     $connect=new PDO($dsn,$user,$pass);
     $connect->exec("SET character_set_connection = 'utf8'");
     $connect->exec("SET NAMES 'UTR8'");
-    } catch (Exception $error) {
+    } catch (PDOException $error) {
 
         echo "unable to connect".$error->getMessage();
     }
